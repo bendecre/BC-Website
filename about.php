fedcf4ce2469ad0d2557a1582a0327479ce1c083
Benjamin Decre <bendecre@comcast.net>
2019-07-22 14:32:42 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+	<head>
+		<base target="_blank">
+		
+		<style>
+			body {
+				font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
+				font-weight: 400;
+				display:table-cell; text-align:center; vertical-align:middle;
+			}
+			
+			p { width: 600px; }
+			
+			div {
+				width: 600px;
+				height: 600px;
+
+				position: absolute;
+				top:0;
+				bottom: 0;
+				left: 0;
+				right: 0;
+
+				margin: auto;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<div><h1 style="color: #593de5;">About</h1>
+			<p>The Institute of Marine and Environmental Technology (IMET) is a joint University System of Maryland research institute capitalizing on the strengths of the <a href="http://www.umces.edu/">University of Maryland Center for Environmental Science</a>, the <a href="http://www.umbc.edu/">University of Maryland Baltimore County</a> and the <a href="http://www.umaryland.edu/">University of Maryland Baltimore</a>. IMET utilizes the research, training and technology transfer capabilities of these partner institutes to further its mission. IMET builds on the assets and capabilities of the former Center of Marine Biotechnology.</p>
+			<p>The scientists at IMET conduct marine and environmental research and thereby create technologies designed to foster the protection and restoration of coastal marine systems and their watersheds, sustainable use of their resources and improvement of human health. IMET's work includes the use of aquaculture and genomics to foster conservation and creation of marine resources, including marine bioenergy; environmental sensor development; environmental remediation; developmental biology; marine biomedicine; molecular and cellular systems and sustainable urban ports and ecosystems.</p>
+		</div>
+	</body>
+</html>
